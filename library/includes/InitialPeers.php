d35ba93d415fb611ca54825e0b9342d74811bade
angelexevior <exevior@localhost>
2022-02-22 16:07:14 +0200

    Repo clean deprecated files

--- library/includes/InitialPeers.php
+++ library/includes/InitialPeers.php
@@ -9,12 +9,12 @@ final class InitialPeers
 {
     public const MINIMUM_PEERS_REQUIRED = 1;
     public const PRELOAD_ERROR = 'Unable to retrieve peers from the preload list.';
-    public const PRELOAD_LIST = 'https://peer1.steroid.io';
+    public const PRELOAD_LIST = 'http://peer1.steroid.io';
 
     /**
      * @var array
      */
-    private $peerList = ['https://peer1.steroid.io'];
+    private $peerList = ['http://peer1.steroid.io'];
 
     /**
      * InitialPeers constructor.
