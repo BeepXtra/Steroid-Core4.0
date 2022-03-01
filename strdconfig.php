f2cad5a0b58aecdd43a6a247ad15a10f068d3150
exevior <exevior@localhost>
2022-03-01 11:37:52 +0200

    Merge origin/master
    Conflicts:
    	doc/api_data.js
    	doc/utils/send_sample_request.js
    	index.php

--- strdconfig.php
+++ strdconfig.php
@@ -4,9 +4,9 @@ class strdconfig {
 
     //Database configuration
     public $db_hostname = 'localhost';
-    public $db_username = 'global';
-    public $db_password = 'WwaHf8hYJrxwXxR9';
-    public $strd_database = 'S4QL';
+    public $db_username = 'testnet';
+    public $db_password = '1QHUGrl7mCQ33uSg';
+    public $strd_database = 'testnet';
     public $debug = 1;
     //The time a session should be left alive (In seconds)
     //This is for security reasons. Users will be automatically logged out after the specified seconds of inactivity
