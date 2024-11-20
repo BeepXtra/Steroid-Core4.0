82b5a1d7a202d5cd0301f165ec33598007e6bdeb
angelexevior <angelos@exevior.com>
2024-11-20 13:20:03 +0200

    SPool class creation provisioning mining segmentation and global pool integration

--- library/classes/SPool.php
+++ library/classes/SPool.php
@@ -7,34 +7,9 @@ defined('_SECURED') or die('Restricted access');
  */
 class SPool {
     
-    public $nonce;
-    public $timestamp;
-    public $data;
-    public $index;
-    public $hash;
-    public $previoushash;
-    
-    //Used when creating a new block
-    function block($index, $timestamp, $data, $previoushash = null){
-        $this->index = $index;
-        $this->timestamp = $timestamp;
-        $this->data = $data;
-        $this->previousHash = $previoushash;
-        $this->hash = $this->calculateHash();
-        $this->nonce = 0;
-
-    }
-    
-    /**
-     * Hash encryption algorithm
+    /* 
+     * Dormant function for future segmentation of node mining and pooled mining
      */
-    public function calculateHash()
-    {
-        //Using sha256. Could be altered with any encryption algorithm
-        //*tip For php the argon2 library is fast and efficient
-        return hash("sha512", $this->index.$this->previousHash.$this->timestamp.((string)$this->data).$this->nonce);
-    }
     
    
 }
-?>
